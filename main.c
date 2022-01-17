734da4f354027e81e60bcde3f9c793e4252e30b5
neshaalaksh <scientistneshaa@gmail.com>
2022-01-17 17:12:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
-
-
-//hi
+#include <string.h>
 
 //functions
 void start(){
     int op;
 startloop:
-    printf("\nOptions :\n\t[1]Registeration\n\t[2]Check\n\t[3]Count\n\t[4]Export details\nEnter any option number :");
+    printf("\nOptions :\n\t[1]Registeration\n\t[2]Check\n\t[3]Count\nEnter any option number :");
     scanf("%d",&op);
-    switch (op){
-        case 1:
-            reg();
-            break;
-        case 2:
-            check();
-            break;
-        case 3:
-            count();
-            break;
-        case 4:
-            exp();
-            break;
-        default:
-            printf("\nEnter only 1,2,3 or 4");
-            goto startloop;
-    }
-
+    if (op==1) reg();
+    else if (op==2) check();
+    else if (op==3) count();
+    else {printf("\nEnter only 1,2 or 3");
+        goto startloop;}
 }
-
 
 void reg(){
     int op;
@@ -74,7 +58,7 @@ checkloop:
 void count(){
     int op;
 countloop:
-    printf("\nOptions :\n\t[1]Count of dose 1 completion\n\t[2]Count of dose 2 completion\n\t[3]Count of booster dose completion\n\t[4]Count of Teenages/Adults/Senior citizens\n\t[5]Count of Female/Male/Other gender");
+    printf("\nOptions :\n\t[1]Count of dose 1 completion\n\t[2]Count of dose 2 completion\n\t[3]Count of booster dose completion\n\t[4]Count of Female/Male/Other gender");
     scanf("%d",&op);
     switch (op){
         case 1:
@@ -86,85 +70,104 @@ countloop:
         case 3:
             ccdoseb();
             break;
-        case 4:
-            ctas();
-            break;
-        case 5:
-            cfmo();
-            break;
+//        case 4:
+//            cfmo();
+//            break;
         default:
-            printf("\nEnter only 1,2,3,4 or 5");
+            printf("\nEnter only 1,2,3 or 4");
             goto countloop;
     }
 }
-void exp(){
-//    export as text file
-    printf("exp");
-}
 
-void regdose1(){
-    int phonenum;
-    printf("\nEnter Phone number : ");
-    scanf("%d",&phonenum);
-    if(checkdose1(phonenum)==1){
-        getdetails(in); //how to get the array end number
-    }else{
-        printf("\nPhone number already linked with another registered patient");
-    }
-}
 
-void regdose2(){
-    checkdose2()
+int regdose1(){
+    inputDetails();
+    fileWriter("dose1");
+    return 0;
 }
-
-void regdoseb(){
-    checkdoseb();
+int regdose2(){
+    inputDetails();
+    fileWriter("dose2");
+    return 0;
 }
-
+int regdoseb(){
+    inputDetails();
+    fileWriter("doseb");
+}
 int checkdose1(int phonenum){
-    for(int i;i<1000;i++){
-        if(information1[i].phonenum == phonenum){
-            return 1;
-        }else return 0;
-    }
 }
-
 int checkdose2(int phonenum){
-    for(int i;i<1000;i++){
-        if(information2[i].phonenum == phonenum){
-            return 1;
-        }else return 0;
-    }
 }
 int checkdoseb(int phonenum){
-    for(int i;i<1000;i++){
-        if(informationb[i].phonenum == phonenum){
-            return 1;
-        }else return 0;
-    }
+
 }
 
-void getdetails(char *name,*address,*gender, int *age,*phonenum,*aadharnum){
-    
+int ccdose1(){
+    return fileCounter("dose1");
+}
+int ccdose2(){
+    fileCounter("dose2");
+}
+int ccdoseb(){
+    fileCounter("doseb");
+}
+
+
+
+//File Functions
+int fileCounter(char dosen[100]){
+    FILE * fptr;
+    char fname[200]="//Users//neshaa.laksh//Desktop//College//Codes//C//cproject//cproject//cproject//";
+    strcat(fname,dosen);
+    strcat(fname,".txt");
+    int count;
+    fptr=fopen(fname,"r");
+    for ( c = getc(fptr); c != EOF; c = getc(fptr)){
+        if (c == '\n'){
+            count = count + 1;}
+    }
+    fclose(fptr);
+    return count;
+}
+
+void fileWriter(char dosen[100]){
+    FILE * fptr;
+  char fname[200]="//Users//neshaa.laksh//Desktop//College//Codes//C//cproject//cproject//cproject//";
+    strcat(fname,dosen);
+    strcat(fname,".txt");
+    fptr=fopen(fname,"a");
+    fprintf(fptr,"%s",information.all);
+    fclose(fptr);
+}
+void inputDetails(){
+    printf("\nEnter your name : ");
+    scanf("%s",&information.name);
+    printf("\nEnter your Phone number : ");
+    scanf("%s",&information.phonenum);
+    printf("\nGender\n Options\n\t[F]Female\n\t[M]Male\n\t[O]Other gender\nEnter : ");
+    scanf("%s",&information.gender);
+    printf("\nEnter your Age : ");
+    scanf("%s",&information.age);
+    printf("\nEnter your Aadhar number : ");
+    scanf("%s",&information.aadharnum);
+    strcat(information.all,"\n");
+    strcat(information.all,information.name);
+    strcat(information.all,"-");
+    strcat(information.all,information.age);
+    strcat(information.all,"-");
+    strcat(information.all,information.gender);
+    strcat(information.all,"-");
+    strcat(information.all,information.aadharnum);
+    strcat(information.all,"-");
+    strcat(information.all,information.phonenum);
 }
 //Structures
-struct dose1{
-    char name[100],address[500],gender[100];
-    int age,phonenum,aadharnum;
-}information1[1000];
-
-struct dose2{
-    char name[100],address[500];
-    int age,phonenum,aadharnum;
-}information2[1000];
-
-struct boosterdose{
-    char name[100],address[500];
-    int age,phonenum,aadharnum;
-}informationb[1000];
-
+struct dose{
+    char name[100],gender[100],all[1000],age[20],phonenum[20],aadharnum[20];
+}information;
 
 void main() {
     printf("Welcome!!!");
     start();
+    
 }
